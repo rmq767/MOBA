d41f3346bf2dd4512788d28a4b9e8f7406c0fbcd
rmq767 <2501706076@qq.com>
2020-02-13 19:00:05 +0800

    upload url

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -55,7 +55,7 @@ module.exports = app => {
     });
     app.post("/admin/api/upload", authMidWare(), upload.single("file"), async (req, res) => {
         const file = req.file;
-        file.url = `http://localhost:3000/uploads/${file.filename}`;
+        file.url = `http://www.xiaoruan-test.top/uploads/${file.filename}`;
         res.send(file);
     });
 
